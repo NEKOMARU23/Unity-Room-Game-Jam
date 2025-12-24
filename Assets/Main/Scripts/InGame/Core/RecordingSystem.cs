42876c5fe7dd0110d5529e70aca72b28b4da9f6b
motokjy <kjtmtk@icloud.com>
2025-12-24 16:24:37 +0900

    録画用スクリプトの追加

---
+++ Assets/Main/Scripts/InGame/Core/RecordingSystem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Main.InGame.Core
+{
+    public class RecordingSystem : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
