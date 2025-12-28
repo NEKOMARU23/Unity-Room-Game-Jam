fe9b0c761c71c67a1d96e4284767dfe699071a6f
NEKOMARU23 <nekosuki29@icloud.com>
2025-12-28 19:13:37 +0900

    簡易的なリセットの作成

--- Assets/Main/Scripts/InGame/GameGimmick/SimpleReset.cs
+++ Assets/Main/Scripts/InGame/GameGimmick/SimpleReset.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // シーン管理用
+using UnityEngine.InputSystem;    // Input System用
 
 public class SimpleReset : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Rキーが押された瞬間だけ判定
+        if (Keyboard.current != null && Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            // 今開いているシーンを最初からロードし直す
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
